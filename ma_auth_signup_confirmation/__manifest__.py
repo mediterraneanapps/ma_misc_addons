26d9708353647d36eba8c5c3166468934c43266b
mediterraneanapps <58656212+mediterraneanapps@users.noreply.github.com>
2020-01-28 13:20:07 +0100

    Add files via upload

--- ma_auth_signup_confirmation/__manifest__.py
+++ ma_auth_signup_confirmation/__manifest__.py
@@ -2,14 +2,14 @@
     'name': 'Email confirmation on sign up',
     'summary': """New user is able to login only after confirming his/her email""",
     'version': '1.0.1',
-    'author': 'Mediterranean Apps',
+    'author': '`Mediterranean Apps',
     'license': 'LGPL-3',
-    "price": 26.00,
+    "price": 30.00,
     "currency": "EUR",
     'depends': [
         'auth_signup',
     ],
     'data': ['data/config.xml', 'views/thankyou.xml', 'data/email.xml'],
-    'installable': True,
+    'installable': False,
     'post_init_hook': 'init_auth',
 }
